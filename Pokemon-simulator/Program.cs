d2a8cd600e1f63e092a21b7938a11831657f05d4
Martin Olsson <martinolsson89@gmail.com>
2025-11-11 16:19:23 +0100

    Add project files.

---
+++ Pokemon-simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon_simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
